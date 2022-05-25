44d16e61685fd026dd6c5b9db9d8e86acbbef4b4
dannyfrelink <danny-frelink@hotmail.com>
2022-05-25 11:20:57 +0200

    include sockets for real time coordinations

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5151;
-// const fetch = require('node-fetch');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const InfluxDatabase = require('@influxdata/influxdb-client');
+const InfluxDB = InfluxDatabase.InfluxDB;
+const INFLUXDB_URL = 'https://gc-acc.antst.net';
+const INFLUXDB_ORG = 'grca';
+const INFLUXDB_KEY = 'QvDOolmSU478M5YkeD17nVeFb4FA_ngo-P0LNokCe6dS2Y10hxIa1zoQ1ZZ9RipKIds-TO7at1-Wgh7Qi44gAQ==';
+const client = new InfluxDB({ url: INFLUXDB_URL, token: INFLUXDB_KEY });
+const queryApi = client.getQueryApi(INFLUXDB_ORG);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -14,21 +25,16 @@ app.use((req, res) => {
     res.status(404).send('Sorry, deze pagina kon ik niet vinden.');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
 
-
-const InfluxDatabase = require('@influxdata/influxdb-client');
-const InfluxDB = InfluxDatabase.InfluxDB;
-
-const INFLUXDB_URL = 'https://gc-acc.antst.net';
-const INFLUXDB_ORG = 'grca';
-const INFLUXDB_KEY =
-    'QvDOolmSU478M5YkeD17nVeFb4FA_ngo-P0LNokCe6dS2Y10hxIa1zoQ1ZZ9RipKIds-TO7at1-Wgh7Qi44gAQ==';
-
-const client = new InfluxDB({ url: INFLUXDB_URL, token: INFLUXDB_KEY });
-const queryApi = client.getQueryApi(INFLUXDB_ORG);
+io.on('connection', (socket) => {
+    socket.on('location', (coordinations) => {
+        console.log(coordinations.latitude)
+        console.log(coordinations.longitude)
+    })
+});
 
 const groupBy = (items, prop) => {
     return items.reduce((out, item) => {
@@ -40,21 +46,24 @@ const groupBy = (items, prop) => {
 };
 
 async function getData() {
-    const query = `
-    from(bucket: "elmap")
-      |> range(start: now(), stop: 25h)
-      |> filter(fn: (r) => r["_measurement"] == "forecast")
-      |> filter(fn: (r) => r["kind"] == "powerConsumptionBreakdown")
-      |> filter(fn: (r) => r["zone"] == "NL")
-      |> filter(fn: (r) => r["timeoffset"] == "baseline")
-      |> aggregateWindow(every: 1h, fn: mean, createEmpty: false)
-      |> sort(columns: ["_time"], desc: false)
-      |> yield(name: "mean")
-    `;
+    const query = `from(bucket: "providers")
+    |> range(start: -28h, stop: -24h)
+    |> filter(fn: (r) => r["_measurement"] == "past_providers")`;
+
+    // const query = `from(bucket: "elmap")
+    // |> range(start: now(), stop: 48h)
+    // |> filter(fn: (r) => r["_measurement"] == "forecast")
+    // |> filter(fn: (r) => r["kind"] == "powerConsumptionBreakdown")
+    // |> filter(fn: (r) => r["zone"] == "NL")
+    // |> filter(fn: (r) => r["timeoffset"] == "baseline")
+    // |> aggregateWindow(every: 1h, fn: mean, createEmpty: false)
+    // |> sort(columns: ["_time"], desc: false)
+    // |> yield(name: "mean")`;
+
     try {
         const rows = await queryApi.collectRows(query);
         const data = Object.entries(groupBy(rows, "_field"));
-        console.log()
+        // data.forEach(d => console.log(d))
         return data;
     } catch (error) {
         console.error(error);
@@ -62,11 +71,3 @@ async function getData() {
     }
 }
 getData()
-
-
-
-
-
-
-// const rows = queryApi.collectRows(query);
-// const data = Object.entries(groupBy(rows, "_field"));
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,24 @@
-// const InfluxDB = require('@influxdata/influxdb-client');
+const socket = io();
 
-// const INFLUXDB_URL = 'https://gc-acc.antst.net';
-// const INFLUXDB_ORG = 'grca';
-// const INFLUXDB_KEY =
-//     'QvDOolmSU478M5YkeD17nVeFb4FA_ngo-P0LNokCe6dS2Y10hxIa1zoQ1ZZ9RipKIds-TO7at1-Wgh7Qi44gAQ==';
+var div = document.getElementById("location");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        div.innerHTML = "The Browser Does not Support Geolocation";
+    }
+}
 
-// const client = new InfluxDB({ url: INFLUXDB_URL, token: INFLUXDB_KEY });
-// const queryApi = client.getQueryApi(INFLUXDB_ORG);
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    socket.emit('location', { latitude, longitude })
+    div.innerHTML = "Latitude: " + latitude + "<br>Longitude: " + longitude;
+}
 
-// console.log(queryApi)
+function showError(error) {
+    if (error.PERMISSION_DENIED) {
+        div.innerHTML = "The User have denied the request for Geolocation.";
+    }
+}
+getLocation();
