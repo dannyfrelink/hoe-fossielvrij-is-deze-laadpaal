b2003d3d2ec51f73d908c8f0004c865be6d1dff5
dannyfrelink <danny-frelink@hotmail.com>
2022-06-07 13:15:52 +0200

    sustainability score with %

--- public/js/script.js
+++ public/js/script.js
@@ -36,51 +36,47 @@ socket.on('fill-in-data', stations => {
     chargingStations.classList.remove('hidden');
     loaderSection.classList.add('hidden');
 
-    stations.map(station => {
-        Object.keys(station).map(operator => {
-            station[operator].stations.map(stat => {
-                fillInChargingStations(station, operator, stat);
-                radiusFilter.addEventListener('change', () => {
-                    fillInChargingStations(station, operator, stat)
-                });
-            });
-        });
+    fillInChargingStations(stations);
+    radiusFilter.addEventListener('change', () => {
+        fillInChargingStations(stations)
     });
 });
 
-const fillInChargingStations = (station, operator, stat) => {
-    if (stat.distance < radiusFilter.value) {
+const fillInChargingStations = (stations) => {
+    const baseValue = Object.values(stations[0])[0].value;
 
-        let latitude = stat.coordinates.latitude;
-        let longitude = stat.coordinates.longitude;
+    return stations.map(station => {
+        return Object.keys(station).map(operator => {
+            return station[operator].stations.map(stat => {
+                if (stat.distance < radiusFilter.value) {
+                    let latitude = stat.coordinates.latitude;
+                    let longitude = stat.coordinates.longitude;
 
-        // Create each charging station
-        let chargingStation = document.createElement('a');
-        chargingStation.setAttribute('href', `http://www.google.com/maps/place/${latitude},${longitude}`);
-        chargingStation.setAttribute('target', '_blank');
+                    // Create each charging station
+                    let chargingStation = document.createElement('a');
+                    chargingStation.setAttribute('href', `http://www.google.com/maps/place/${latitude},${longitude}`);
+                    chargingStation.setAttribute('target', '_blank');
 
-        // Create content of each charging station
-        let distanceText = document.createElement('h2');
-        distanceText.textContent = `${stat.distance} meters`;
-        let operatorText = document.createElement('p');
-        operatorText.textContent = stat.operatorName;
-        let sustainabilityScore = document.createElement('p');
-        sustainabilityScore.textContent = 'Sustainability score: ';
-        let sustainabilityValue = station[operator].value;
-        if (sustainabilityValue <= 150) {
-            sustainabilityScore.classList.add('good_sustainability');
-        } else if (sustainabilityValue > 150 && sustainabilityValue <= 250) {
-            sustainabilityScore.classList.add('decent_sustainability');
-        } else if (sustainabilityValue > 250 && sustainabilityValue <= 350) {
-            sustainabilityScore.classList.add('bad_sustainability');
-        } else if (sustainabilityValue > 350) {
-            sustainabilityScore.classList.add('terrible_sustainability');
-        }
-        let button = document.createElement('button');
+                    // Create content of each charging station
+                    let distanceText = document.createElement('h2');
+                    distanceText.textContent = `${stat.distance} meters`;
+                    let operatorText = document.createElement('p');
+                    operatorText.textContent = stat.operatorName;
+                    let sustainabilityScore = document.createElement('p');
+                    let sustainabilityValue = station[operator].value;
+                    if (sustainabilityValue == baseValue) {
+                        sustainabilityScore.textContent = 'Sustainability score: 100%';
+                    } else {
+                        sustainabilityScore.textContent = `Sustainability score: ${Math.round(baseValue / sustainabilityValue * 100)}%`
+                    }
+                    let button = document.createElement('button');
 
-        // Append children
-        chargingStation.append(distanceText, operatorText, sustainabilityScore, button);
-        chargingStations.appendChild(chargingStation);
-        return chargingStations;
-    }
+                    // Append children
+                    chargingStation.append(distanceText, operatorText, sustainabilityScore, button);
+                    chargingStations.appendChild(chargingStation);
+                    return chargingStations;
+                }
+            });
+        });
+    });
 }
