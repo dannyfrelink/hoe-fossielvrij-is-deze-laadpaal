50cf0e92f01f7f77bdc4460283c9cc6a7370caea
dannyfrelink <danny-frelink@hotmail.com>
2022-06-22 09:53:34 +0200

    fix preferences screen

--- public/js/script.js
+++ public/js/script.js
@@ -7,12 +7,14 @@ if (window.location.pathname == '/search') {
     const times = document.querySelector('#times');
     const rangeInput = document.querySelector('input[type="range"]');
     const numberInput = document.querySelector('input[type="number"]');
-    const sortInputs = document.querySelectorAll('#filters div:first-of-type input[type="radio"]');
-    const sortInputsAvailability = document.querySelectorAll('#filters div:nth-of-type(2) input[type="radio"]');
+    const sortInputsAvailability = document.querySelectorAll('#filters div:first-of-type input[type="radio"]');
+    const sortInputs = document.querySelectorAll('#filters div:nth-of-type(2) input[type="radio"]');
     const errorMessage = document.querySelector('#error_message');
     const chargingStations = document.querySelector('#charging_stations');
     const loaderSection = document.querySelector('#loader');
     const loaderText = document.querySelector('#loader_text');
+
+    console.log(sortInputs)
 
     const getLocation = () => {
         if (navigator.geolocation) {
